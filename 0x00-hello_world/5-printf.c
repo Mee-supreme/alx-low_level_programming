b4607bbaefd109a535f92d3db4c9b65e532cec39
Mee-supreme <nyaa59424@gmail.com>
2022-09-11 15:30:57 -0700

    Tf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,9 +8,8 @@
  * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
- int main(void)
- {
-	 printf("with proper grammar, but the outcome is a piece of art,\n");
-	 return (0);
- }
-
+int main(void)
+{
+       	printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
