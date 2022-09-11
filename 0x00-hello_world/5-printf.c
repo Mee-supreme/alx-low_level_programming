139bd715d33b531ad4648b7e540f55b3fc0ada47
Mee-supreme <nyaa59424@gmail.com>
2022-09-11 11:21:43 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - print the string in the put function
  *
- * Return: Always 0 (Success)
+ * Description: using the main function
+ *
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
  */
-
  int main(void)
-{       char printfVal[] = "with proper grammar, but the outcome is a piece of art,";
+ {
+	 printf("with proper grammar, but the outcome is a piece of art,\n");
+	 return (0);
+ }
 
-	printf("%s\n", printfVal);
-	return (0);
-}
