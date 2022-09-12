53786995cca9a5e9f28027f7fe9b47614e5c380b
Mee-supreme <nyaa59424@gmail.com>
2022-09-12 04:33:27 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,15 @@
-/*
- * File: 101-quote.c
- * Auth: Muhammed Abdulrahaman Adinoyi
- */
-
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *                   followed by a new line, to standard error.
- *
- * Return: Always 1.
+ * main - Entry point
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-		59);
-		
-	return (1);
+	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
+	puts(c);
+
+	return (1);
 }
