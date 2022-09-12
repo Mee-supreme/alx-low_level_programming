221611dcf374a59206e354269da8cdc73186073c
Mee-supreme <nyaa59424@gmail.com>
2022-09-12 01:52:11 -0700

    4.C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 
-
-
 /**
-
  * main - Entry point
-
  *
-
  * Return: Always 0 (Success)
-
  */
 
-
-
 int main(void)
-
 {	char putsVal[] = "\"Programming is like building a multilingual puzzle";
-
+        
 	puts(putsVal);
 
 	return (0);
