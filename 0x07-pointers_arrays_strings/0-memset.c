b9080c10363ef583628f190d75d7f17a954c547a
Mee-supreme <nyaa59424@gmail.com>
2022-09-27 04:30:26 -0700

    CRAching

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
 /**
  * _memset - fills a memory block with a constant byte
  * @s: address to memory block
- * @b: char to used
+ * @b: char to be used
  * @n: number of bytes to be used
  *
  * Return: pointer to the memory block
@@ -13,8 +13,8 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	while (n)
 	{
-		s[n -1] = b;
-		n==;
+		s[n - 1] = b;
+		n--;
 	}
 	return (s);
 }
