d1c55faad1c0a38a5a3d7e48ded841e297f04d46
Mee-supreme <nyaa59424@gmail.com>
2022-11-10 01:39:05 -0800

    CORDS

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 
 /**
- * print_name -A funtion that prints a name
+ * print_name - A funtion that prints a name
  *
  * @name: pointer to name to print
  * @f: function to pointer to print name
  *
- * Return: no return.
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
